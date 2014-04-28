14d08a56d748d1431e2be40ecd88f17789bf023d
da0shi <dev@da0shi.com>
2014-04-28 23:13:03 +0900

    add custom autoloader. based on fuelphp's autoloader.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,11 +1,15 @@
 <?php
 
 require APPPATH .'vendor/autoload.php';
+require APPPATH .'packages/autoloader.php';
+
+define('DS', DIRECTORY_SEPARATOR);
 
 ORM::configure('mysql:host=localhost;dbname=schedulab;');
 if (PHP_OS === 'Darwin') {
    ORM::configure('mysql:host=localhost;dbname=schedulab;unix_socket=/tmp/mysql.sock;');
 }
-
 ORM::configure('username', 'schedulab');
 ORM::configure('password', 'schedulab');
+
+Autoloader::register();
