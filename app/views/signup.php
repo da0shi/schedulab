6a04249ca99d93b48b9273ddbee6d3d2785a1f25
da0shi <dev@da0shi.com>
2014-05-12 19:53:11 +0900

    fixed to use functions from Slim to 'request' and 'session flash'
    Slim already had functioins to "get request value" and "set flash
    message by session". Therefore I remove self custom functions.
    And remove session function from Model class because session is a
    function for View and Controller, but not Model

--- app/views/signup.php
+++ app/views/signup.php
@@ -5,24 +5,30 @@
 	<title><?= $title ?></title>
 </head>
 <body>
+<div class="container">
 	<h1>User Register</h1>
 	<p>
 <div class="errors">
-<?php if (isset($duplicate)) echo $duplicate; ?>
-<?php if (isset($shortpass)) echo $shortpass; ?>
+<?php echo $flash['emptyval']; ?>
 </div>
 		<form action="" method="post">
+<section>
 			<label for="name">username:</label>
-			<input type="text" name="name" id="name" />
-			</br>
+			<input type="text" name="name" id="name" required />
+</section>
+<section>
 			<label for="email">email:</label>
-			<input type="text" name="email" id="email" />
-			</br>
+			<input type="email" name="email" id="email" required />
+			<?php echo $flash['duplicate']; ?>
+</section>
+<section>
 			<label for="password">password:</label>
-			<input type="password" name="password" id ="password" />
-			</br>
+			<input type="password" name="password" id ="password" required />
+			<?php echo $flash['shortpass']; ?>
+</section>
 			<input type="submit" value="登録" />
 		</form>
 	</p>
+</div><!-- .container -->
 </body>
 </html>
