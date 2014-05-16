732fe9e2dee5502366a144eb8caa080abbc93b1e
taikihomma <taikihomma0509@gmail.com>
2014-05-16 18:22:47 +0900

    commit to merge.

--- app/views/signup.php
+++ app/views/signup.php
@@ -16,7 +16,7 @@
 			<input type="text" name="name" id="name" />
 			</br>
 			<label for="email">email:</label>
-			<input type="text" name="email" id="email" />
+			<input type="email" name="email" id="email" />
 			</br>
 			<label for="password">password:</label>
 			<input type="password" name="password" id ="password" />
