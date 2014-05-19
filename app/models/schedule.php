3f6484246f09a57311929475389d00c3cf87c73e
taikihomma <taikihomma0509@gmail.com>
2014-05-19 19:56:59 +0900

    modify error messages.

--- app/models/schedule.php
+++ app/models/schedule.php
@@ -13,4 +13,47 @@ class Schedule extends Model
 	{
 		return $this->has_many('UserSchedule');
 	}
+
+	public static function create ($args) 
+	{
+		$errorMssgs = array('hasError' => false);
+		if (strlen($args['title']) == 0) {
+			$errorMssgs['empty-title'] = 'タイトルが空です';
+			$errorMssgs['hasError'] = true;
+		}
+		if (isset($args['allday'])) {
+			$args['allday'] = false;
+			if (strlen($args['startDate']) == 0 || strlen($args['endDate']) == 0) {
+				$errorMssgs['empty-date'] = '日付を入力してください';
+				$errorMssgs['hasError'] = true;
+			}
+		} else {
+			$args['allday'] = true;
+			if (strlen($args['startDate']) == 0 || strlen($args['startTime']) == 0 || strlen($args['endDate']) == 0 || strlen($args['endTime']) == 0) {
+				$errorMssgs['empty-date'] = '日付を入力してください';
+				$errorMssgs['hasError'] = true;
+			}
+		}
+
+		$start = $args['startDate'] . " "  . $args['startTime'];
+		$end = $args['endDate'] . " "  . $args['endTime'];
+		if (strtotime($start) > strtotime($end)) {
+			$errorMssgs['invalid-date'] = '日付が不正です';
+			$errorMssgs['hasError'] = true;
+		}
+		
+		if ($errorMssgs['hasError']) return $errorMssgs;
+		
+		$schedule = Model::factory('Schedule')->create();
+		#$schedule->user_id = ;
+		$schedule->title = $args['title'];
+		$schedule->description = $args['detail'];
+		$schedule->is_allday = $args['allday'];
+		$schedule->start_at = $start;
+		$schedule->end_at = $end;
+		$schedule->create_at = date('Y-m-d H:i:s');
+		$schedule->update_at = date('Y-m-d H:i:s');
+		return $schedule->save();
+	}
+
 }
